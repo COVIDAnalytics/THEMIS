bf46616994d6761257c003a98c757f3c460b510c
Saksham Soni <saksham@Sakshams-MacBook-Pro.local>
2021-03-14 10:52:06 -0400

    changed policy labels

--- cost_functions/economic_cost/economic_data/economic_params.py
+++ cost_functions/economic_cost/economic_data/economic_params.py
@@ -5,11 +5,11 @@ UNEMPLOYMENT_COST = {"GM": 77510,"US": 100000}
 # employment_impact in percentages
 EMPLOYMENT_IMPACT = {"GM": { "Lockdown": 0.02,
         "Social Distancing": 0.01,
-        "None": 0}}
+        "No_Measure": 0}}
 
 GDP_IMPACT = {"GM": { "Lockdown": 0.10,
         "Social Distancing": 0.05,
-        "None": 0}}
+        "No_Measure": 0}}
 
 
 TOTAL_LABOR_FORCE = {"GM": 43356000}
