6efca48dd064b144ee81fc35045b74435c54eab3
[BabelRO] <[baptiste.rossi@m4x.org]>
2021-03-15 22:00:35 +0100

    Mental health cost

--- pandemic_functions/pandemic.py
+++ pandemic_functions/pandemic.py
@@ -7,14 +7,14 @@ class Pandemic:
     # We call it here so that we dont have to repeatedly call DELPHI over and over again. 
         self.policy = policy
         self.region = region
-        self.num_deaths, self.hospitalization_days, self.icu_days, self.ventilated_days = self._get_deaths_and_hospitalizations()        
+        self.num_deaths, self.hospitalization_days, self.icu_days, self.ventilated_days, self.num_cases = self._get_deaths_and_hospitalizations()        
         
         
     def _get_deaths_and_hospitalizations(self):
         # this function gets the number of deaths and hospitalizations that would occur under such policy, using DELPHI
         # the return value is a tuple of numbers
         # TODO: This is current a dummy function
-        return 60000, 1000000, 200000, 100000
+        return 60000, 1000000, 200000, 100000, 2400000
         
         
 
