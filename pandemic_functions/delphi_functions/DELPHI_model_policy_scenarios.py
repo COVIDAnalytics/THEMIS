pandemic hosp calculation fix + pandemic data

pandemic_functions/delphi_functions/DELPHI_model_policy_scenarios.py
@@ -11,17 +11,7 @@ from scipy.integrate import solve_ivp
 from datetime import datetime, timedelta
 from dateparser import parse
 from dateutil.relativedelta import relativedelta
-# from DELPHI_utils_V4_static import compute_mape, create_fitting_data_from_validcases, get_mape_data_fitting, DELPHIAggregations
-# from DELPHI_utils_V4_dynamic import (
-#     read_oxford_international_policy_data, get_normalized_policy_shifts_and_current_policy_all_countries,
-#     get_normalized_policy_shifts_and_current_policy_us_only, read_policy_data_us_only
-# )
-# from DELPHI_params_V4 import (
-#     fitting_start_date,
-#     date_MATHEMATICA, validcases_threshold_policy, default_dict_normalized_policy_gamma,
-#     IncubeD, RecoverID, RecoverHD, DetectD, VentilatedD,
-#     default_maxT_policies, p_v, future_policies, future_times
-# )
+
 from pandemic_functions.pandemic_params import (
     default_dict_normalized_policy_gamma, future_policies,
     p_d, p_h, p_v,
@@ -40,41 +30,8 @@ import os
 import argparse
 
 
-#%%
+popcountries = pd.read_csv("pandemic_functions/pandemic_data/Population_Global.csv")
 
-# USER_RUNNING = RUN_CONFIG["arguments"]["user"]
-# OPTIMIZER = RUN_CONFIG["arguments"]["optimizer"]
-# GET_CONFIDENCE_INTERVALS = bool(int(RUN_CONFIG["arguments"]["confidence_intervals"]))
-# SAVE_TO_WEBSITE = bool(int(RUN_CONFIG["arguments"]["website"]))
-# SAVE_SINCE100_CASES = bool(int(RUN_CONFIG["arguments"]["since100case"]))
-# PATH_TO_FOLDER_DANGER_MAP = CONFIG_FILEPATHS["danger_map"][USER_RUNNING]
-# PATH_TO_DATA_SANDBOX = CONFIG_FILEPATHS["data_sandbox"][USER_RUNNING]
-# PATH_TO_WEBSITE_PREDICTED = CONFIG_FILEPATHS["website"][USER_RUNNING]
-# policy_data_countries = read_oxford_international_policy_data(yesterday=yesterday)
-# # policy_data_us_only = read_policy_data_us_only(filepath_data_sandbox=CONFIG_FILEPATHS["data_sandbox"][USER_RUNNING])
-popcountries = pd.read_csv(PATH_TO_FOLDER_DANGER_MAP + f"processed/Global/Population_Global.csv")
-# df_initial_states = pd.read_csv(
-#     PATH_TO_DATA_SANDBOX + f"predicted/raw_predictions/Predicted_model_state_V4_{fitting_start_date}.csv"
-# )
-# subname_parameters_file = None
-# if OPTIMIZER == "tnc":
-#     subname_parameters_file = "Global_V4"
-# elif OPTIMIZER == "annealing":
-#     subname_parameters_file = "Global_V4_annealing"
-# elif OPTIMIZER == "trust-constr":
-#     subname_parameters_file = "Global_V4_trust"
-# else:
-#     raise ValueError("Optimizer not supported in this implementation")
-# yesterday = "20210305"
-# past_parameters = pd.read_csv(
-#     PATH_TO_FOLDER_DANGER_MAP + f"predicted/Parameters_{subname_parameters_file}_{yesterday}.csv"
-# )
-# if pd.to_datetime(yesterday) < pd.to_datetime(date_MATHEMATICA):
-#     param_MATHEMATICA = True
-# else:
-#     param_MATHEMATICA = False
-# # True if we use the Mathematica run parameters, False if we use those from Python runs
-# # This is because the past_parameters dataframe's columns are not in the same order in both cases
 
 def gamma_t(day: datetime, state: str, params_dict: dict) -> float:
     """

pandemic_functions/pandemic.py
@@ -8,7 +8,6 @@ from pandemic_functions.delphi_functions.DELPHI_model_policy_scenarios import ru
 from pandemic_functions.pandemic_params import (
     DELPHI_PATH, 
     PATH_TO_FOLDER_DANGER_MAP,
-    GLOBAL_HOSPITALIZATION_DATA_PATH,
     region_symbol_country_dict,
     p_v
 )
@@ -30,12 +29,10 @@ class Pandemic:
         country = region_symbol_country_dict[self.region]
         country_sub = country.replace(' ', '_')
         if self.policy.policy_type == "actual":
-            if os.path.exists(PATH_TO_FOLDER_DANGER_MAP + f"processed/Global/Cases_{country_sub}_None.csv"):
-                totalcases = pd.read_csv(
-                    PATH_TO_FOLDER_DANGER_MAP + f"processed/Global/Cases_{country_sub}_None.csv"
-                )
+            if os.path.exists(f"pandemic_functions/pandemic_data/Cases_{country_sub}_None.csv"):
+                totalcases = pd.read_csv(f"pandemic_functions/pandemic_data/Cases_{country_sub}_None.csv")
             else:
-                raise FileNotFoundError(f"Can not find file - processed/Global/Cases_{country_sub}_None.csv for actual polcy outcome")
+                raise FileNotFoundError(f"Can not find file - pandemic_data/Cases_{country_sub}_None.csv for actual polcy outcome")
 
             # yesterday = "".join(str(datetime.now().date() - timedelta(days=1)).split("-"))
             if os.path.exists("pandemic_functions/pandemic_data/Global_DELPHI_predictions_combined.csv"):
@@ -56,15 +53,16 @@ class Pandemic:
             ventilated_days = preds_in_interval["Active Ventilated"].sum()
 
             if self.region == "GM":
-                hosp_global = pd.read_csv(GLOBAL_HOSPITALIZATION_DATA_PATH+"global_hospitalizations.csv")
+                hosp_global = pd.read_csv("pandemic_functions/pandemic_data/global_hospitalizations.csv")
                 hosp_global.date = pd.to_datetime(hosp_global.date)
                 hosp_germany = hosp_global[hosp_global.country_id == "DE"]
                 hosp_germany.date = pd.to_datetime(hosp_germany.date)
                 hosp_germany = hosp_germany.query("date >= @start_date and date <= @end_date")
                 icu_days = np.nansum(hosp_germany.icu_beds_used)
+                icu_days = icu_days - ventilated_days
                 hospitalization_days = hospitalization_days - icu_days
             else:
-                icu_days = ventilated_days*(0.15/0.85) # 
+                icu_days = ventilated_days*(0.15/0.85) # (1/0.85 - 1)*ventilated_days
                 hospitalization_days = hospitalization_days - icu_days
         else:
             # TODO: implement hypothetical policy case