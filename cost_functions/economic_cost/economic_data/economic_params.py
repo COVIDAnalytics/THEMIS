cdef13fae0cd0ec4fef44f85d1b0bb7ae6dcb3e2
Michael Li <shaznl1@hotmail.com>
2021-03-31 18:18:45 -0400

    Update economic_params.py

--- cost_functions/economic_cost/economic_data/economic_params.py
+++ cost_functions/economic_cost/economic_data/economic_params.py
@@ -7,14 +7,20 @@ def not_implemented():
 # employment_impact in percentages
 EMPLOYMENT_IMPACT = {}
 EMPLOYMENT_IMPACT["GM"] = defaultdict(not_implemented, { "Lockdown": 2,
+          "Restrict_Mass_Gatherings_and_Schools_and_Others": 1.8,
+          'Authorize_Schools_but_Restrict_Mass_Gatherings_and_Others': 1.5,
+          'Restrict_Mass_Gatherings_and_Schools': 1.2,
         "Restrict_Mass_Gatherings": 1,
         "No_Measure": 0})
 
 GDP_IMPACT = {}
 
 GDP_IMPACT["GM"] = defaultdict(not_implemented, { "Lockdown": -10,
-                                           "Restrict_Mass_Gatherings": -3,
-                                           "No_Measure": 0})
+          "Restrict_Mass_Gatherings_and_Schools_and_Others": -8,
+          'Authorize_Schools_but_Restrict_Mass_Gatherings_and_Others': -6,
+          'Restrict_Mass_Gatherings_and_Schools': -4,
+           "Restrict_Mass_Gatherings": -3,
+           "No_Measure": 0})
 
 
 TOTAL_LABOR_FORCE = {"GM": 43356000}
